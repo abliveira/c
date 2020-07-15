fe88f02a2a6782781c117bf14d010cd13b1ed926
abliveira <41597910+abliveira@users.noreply.github.com>
2020-07-15 10:46:40 -0300

    Update Functions and Array Pointers.c

--- Compound data types/Functions and Array Pointers.c
+++ Compound data types/Functions and Array Pointers.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+// Function pointers apontam para código executável para uma função na memória
+// Os ponteiros de função podem ser armazenados em uma matriz ou transmitidos como argumentos para outras funções
+
 int add (int x, int y) {
   return(x + y);
 }
@@ -21,7 +24,11 @@ int divide (int x, int y) {
 
 int main() {
   int x, y, choice, result;
-  int (*op[4])(int, int) = {add, subtract, multiply, divide};  // Declara o vetor de ponteiros de função
+  
+  // Um ponteiro de função é declarado no seguinte formato: return_type (*func_name)(parameters) 
+  int (*op[4])(int, int) = {add, subtract, multiply, divide};
+  // A associação ao ponteiro à função pode ser feita na declaração, como acima, ou após. Ex: ptr = func;
+  
   printf("Enter two integers: ");
   scanf("%d%d", &x, &y);
   printf("Enter 0 to add, 1 to subtract, 2 to multiply, or 3 to divide: ");
