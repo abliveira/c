4b669c7c7f3975bd8b6f4650a6e1b79b38df2c4f
abliveira <41597910+abliveira@users.noreply.github.com>
2020-08-06 22:08:14 -0300

    Create Working with File.c

---
+++ Others/Working with File.c
@@ -0,0 +1,47 @@
+#include <stdio.h> // Inclui funções de arquivos
+
+/* fopen("file", ?)
+- r open for reading (file must exist)
+- w open for writing (file need not exist, or will be overwrited)
+- a open for append (file need not exist)
+- r+ open for reading and writing from beginning
+- w+ open for reading and writing, overwriting file
+- a+ open for reading and writing, appending to file */
+
+
+#include <stdio.h>
+
+int main() {  
+    FILE *fptr;
+    int stock;
+    char buffer[200], item[10], c;
+    float price;
+    
+    // fopen retorna um ponteiro que será usado para o manejo de arquivos
+    fptr = fopen("myfile.txt", "w");
+    if (fptr == NULL) {   // Se retornar NULL, houve um erro
+        printf("Error opening file.");
+        return 1;
+    }
+    
+    /* myfile.txt: Inventory\n100 Widget 0.29\nEnd of List */
+    fprintf(fptr, "Inventory\n");
+    fprintf(fptr, "%d %s %f\n", 100, "Widget", 0.29);
+    fputs("End of List", fptr); // Writes string str to the file pointed
+    //fputc(char, fp) Writes character char to the file pointed to by fp.
+    fclose(fptr);
+    
+    fptr = fopen("myfile.txt", "r");
+    fgets(buffer, 20, fptr);    //Le a primeira linha e armazena em buffer
+    printf("%s\n", buffer);
+    fscanf(fptr, "%d%s%f", &stock, item, &price);   // Continua da segunda linha, lê item por item separadamente
+    printf("%d  %s  %4.2f\n", stock, item, price);
+    
+    // fgetc() retorna o próximo caracter do arquivo apontado pelo ponteiro
+    // Se o final do arquivo tiver sido alcançado, o EOF será retornado.
+    while ((c = fgetc(fptr)) != EOF) /* read the rest of the file */
+        printf("%c", c);
+    
+    fclose(fptr);
+    return 0;
+}
