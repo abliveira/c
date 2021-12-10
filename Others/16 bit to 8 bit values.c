f03441c4cb9fc96eb4a14335397a5849626f706a
abliveira <41597910+abliveira@users.noreply.github.com>
2021-12-10 12:40:12 -0300

    Update 16 bit to 8 bit values.c

--- Others/16 bit to 8 bit values.c
+++ Others/16 bit to 8 bit values.c
@@ -3,16 +3,18 @@
 int main() {
 
   for (int i = 0; i < 10000; i++) {
-
+    
+    // Gera uma variável de exemplo do tipo int (16 bit)
     int sixteen_bit_val = i;
     printf("%d\t", sixteen_bit_val);
 
+    // Quebra a variável 16 bit em duas de 8 bit (2 bytes)
     short int lo_byte = sixteen_bit_val & 0xFF;
     short int hi_byte = sixteen_bit_val >> 8;
-
     printf("%d\t", lo_byte);
     printf("%d\t", hi_byte);
-
+    
+    // Concatena as duas variáveis em uma novamente
     int restored_sixteen_bit_val = (hi_byte << 8) | (lo_byte & 0xff);
     printf("%d\n", restored_sixteen_bit_val);
   }
