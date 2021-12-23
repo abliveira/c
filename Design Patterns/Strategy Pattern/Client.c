de05429a24e44f6fa2a106433e85d858f5cd298c
“abliveira” <dev.ab@outlook.comgit config --global user.email dev.ab@outlook.com>
2021-12-23 13:01:55 -0300

    Strategy Pattern

---
+++ Design Patterns/Strategy Pattern/Client.c
@@ -0,0 +1,53 @@
+#include "Customer.h"
+#include "CustomerCategories.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+Based on "Software Design Patterns in C - Educative.io"
+
+The example in this chapter started with a naive implementation where we did a switch-case on 
+an enum (CustomerCategorycategory). This approach created control coupling. Since control coupling 
+is a common design flaw, it pays off to understand and avoid it. The main problem with control coupling is
+When we pass a flag to a function, we’re creating a dependency on the inner logic of that function. 
+If our assumptions change, we have to modify multiple parts of our program instead of encapsulating the 
+behavior in one place.
+
+According to the open-closed principle:
+- Extending the behavior of an ideal module is achieved by adding code instead of changing the existing source.
+- Following this principle minimizes the risk of introducing bugs in existing, tested code and typically raises 
+the quality of the design by introducing loose coupling.
+- Unfortunately, it is virtually impossible to design a module so that it is closed against all kinds of changes.
+- Even trying to design software in such a way would overcomplicate the design far beyond suitability.
+- Identifying which modules to close and what changes to close them against requires experience and a good understanding 
+of the problem domain.
+
+Strategy is often a good approach when refactoring code duplication. You have multiple functions with very 
+similar responsibilities. Extracting the varied concepts allows us to reuse the general algorithm. We can 
+now represent each varied concept as a strategy passed to the general and reusable algorithm. This is true and 
+it’s an example of the benefits of a design following the open-closed principle.
+*/
+
+/*
+Client code illustrating the binding of a strategy:
+*/
+static CustomerPtr createBronzeCustomer(
+  const char* name,
+  const Address* address)
+{
+  return createCustomer(name, address, bronzePriceStrategy);
+}
+
+
+int main() {
+
+  char *name = "Client Name"; 
+	Address* addr = malloc(sizeof *addr);
+
+  CustomerPtr newCustomer = createBronzeCustomer(name, addr);
+  printf ("Bronze customer created\n");
+
+  changePriceCategory(newCustomer, silverPriceStrategy);
+  printf ("Customer stratgey changed to Silver\n");
+}
---
+++ Design Patterns/Strategy Pattern/Customer.c
@@ -0,0 +1,63 @@
+#include "Customer.h"
+#include <stdlib.h>
+#include <assert.h>
+
+/*
+I never really dive into the details of these types.
+Let's just create a dummy placeholder for the information.
+*/
+typedef struct
+{
+  unsigned long orderId;
+  double amount;
+  double shipping;
+} Order;
+
+typedef struct
+{
+  int implementationOfLinkedList;
+} List;
+
+struct Customer 
+{
+  const char* name; 
+  Address address; 
+  List orders;
+   
+  /* Bind the strategy to Customer: */
+  CustomerPriceStrategy priceStrategy;
+};
+
+CustomerPtr createCustomer(
+  const char* name, 
+  const Address* address,
+  CustomerPriceStrategy priceStrategy)
+{
+  CustomerPtr customer = malloc(sizeof *customer);
+  
+  if(NULL != customer) {
+    /* Bind the initial strategy supplied by the client. */
+    customer->priceStrategy = priceStrategy; 
+    customer->name= name; 
+    customer->address = *address; 
+/* foo->bar is equivalent to (*foo).bar, i.e. it gets the member called bar from the struct that 
+   foo points to. */
+    /* Initialize the other attributes of the customer here. */
+  }
+
+  return customer; 
+}
+
+void placeOrder(struct Customer* customer, const Order* order)
+{
+  double totalAmount = customer->priceStrategy(order->amount, order->shipping);
+/* foo->bar is equivalent to (*foo).bar, i.e. it gets the member called bar from the struct that 
+   foo points to. */
+  /* More code to process the order... */
+}
+
+void changePriceCategory(CustomerPtr customer, CustomerPriceStrategy newPriceStrategy)
+{
+  assert(NULL != customer);
+  customer->priceStrategy = newPriceStrategy;
+}
